20c800874745abb85bca5c0f66019b96aa17a604
ceefour <ceefour@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2008-03-30 12:54:22 +0000

    Now using WEBrick servlet.
    git-svn-id: http://subversion.assembla.com/svn/xmlrpcbridge/trunk@12 c7ad7044-cb60-498d-a845-55a1d3ae732a

--- unfuddle_xmlrpc/server.rb
+++ unfuddle_xmlrpc/server.rb
@@ -3,6 +3,7 @@
 
 require 'rubygems'
 require 'xmlrpc/server'
+require 'webrick'
 
 $LOAD_PATH << 'lib'
 require 'unfuddle'
@@ -13,9 +14,12 @@ unless ARGV.size >= 4
 end
 
 server_port = (ENV['XMLRPCSERVER_PORT'] || 8080).to_i
-server = XMLRPC::Server.new(server_port)
+server = WEBrick::HTTPServer.new(:Port => server_port) # XMLRPC::Server.new(server_port)
+xs = XMLRPC::WEBrickServlet.new
+server.mount('/', xs)
 trap('INT') { server.shutdown }
 
+puts "Loading account..."
 account = Unfuddle::AccountProxy.new(ARGV[0], ARGV[1], ARGV[2])
 project = account.project.get_by_short_name(ARGV[3])
 ticket = account.ticket(project['id'])
@@ -23,7 +27,7 @@ ticket_interface = XMLRPC::interface('ticket') {
   meth 'array query(string)', 'Query tickets and return ticket IDs'
   meth 'array get(id)', 'Get ticket detail'
 }
-server.add_handler ticket_interface, ticket
+xs.add_handler ticket_interface, ticket
 
 puts "Server URL: http://localhost:#{server_port}/"
 puts "Test using irb:"
@@ -31,4 +35,4 @@ puts "  require 'xmlrpc/client'"
 puts "  proxy = XMLRPC::Client.new2('http://localhost:#{server_port}/')"
 puts "  proxy.call('ticket.query', 'status!=closed')"
 puts "  proxy.call('ticket.get', 1)"
-server.serve
+server.start
